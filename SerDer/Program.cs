d7492a958769c8992463c19abd83fdd5cbcc2320
özge alkan <ozgetanulkualkan@gmail.com>
2023-01-10 15:52:20 +0300

    Add project files.

---
+++ SerDer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace SerDer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kisi k1 = new Kisi { Id=1, Ad="Ali"};
+
+            JsonSerializer serializer = new JsonSerializer();
+            StringWriter sw = new StringWriter();
+            
+            serializer.Serialize(sw,k1);
+            Console.WriteLine(sw.ToString());
+            
+        }
+    }
+    public class Kisi
+    {
+        [JsonProperty()]
+        public int Id { get; set; }
+        public string Ad { get; set; }
+    }
+}
